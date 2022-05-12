6f9c9a382f1a693ccdd0945aa0208e17ae3df27f
archi <arthur_bandaryk@mail.ru>
2022-05-12 09:37:24 +0300

    Rename '.cpp|.hpp' files in 'include/stout' and in 'include/stout/os/posix'

---
+++ include/stout/os/posix/realpath.h
@@ -0,0 +1,43 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+
+#include "stout/error.h"
+#include "stout/result.h"
+
+////////////////////////////////////////////////////////////////////////
+
+namespace os {
+
+////////////////////////////////////////////////////////////////////////
+
+inline Result<std::string> realpath(const std::string& path) {
+  char temp[PATH_MAX];
+  if (::realpath(path.c_str(), temp) == nullptr) {
+    if (errno == ENOENT || errno == ENOTDIR) {
+      return None();
+    }
+
+    return ErrnoError();
+  }
+
+  return std::string(temp);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+} // namespace os
+
+////////////////////////////////////////////////////////////////////////
