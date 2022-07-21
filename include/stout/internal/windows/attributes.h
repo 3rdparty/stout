e99abd1d2db96a784d13fab7918d7263972faaf0
archi <arthur_bandaryk@mail.ru>
2022-07-21 16:27:56 +0300

    Replace manual stringification with 'fmt::format' in 'include/stout/internal/windows' directory

--- include/stout/internal/windows/attributes.h
+++ include/stout/internal/windows/attributes.h
@@ -15,6 +15,12 @@
 #include <string>
 
 #include "stout/error.h"
+// Since 'fmt' library doesn't support 'wide' conversion (e.g
+// from 'std::wstring' to 'std::string' and vice versa) we use
+// API from 'include/stout/stringify.h' (e.g:
+// std::string stringify(const std::wstring& wstr) - function).
+// Check the issue for fmt conversion on github:
+// https://github.com/fmtlib/fmt/issues/1116
 #include "stout/stringify.h"
 #include "stout/try.h"
 #include "stout/windows.h"
--- include/stout/internal/windows/longpath.h
+++ include/stout/internal/windows/longpath.h
@@ -18,6 +18,12 @@
 
 #include "stout/os/constants.h"
 #include "stout/path.h"
+// Since 'fmt' library doesn't support 'wide' conversion (e.g
+// from 'std::wstring' to 'std::string' and vice versa) we use
+// API from 'include/stout/stringify.h' (e.g:
+// std::string stringify(const std::wstring& wstr) - function).
+// Check the issue for fmt conversion on github:
+// https://github.com/fmtlib/fmt/issues/1116
 #include "stout/stringify.h"
 
 ////////////////////////////////////////////////////////////////////////
